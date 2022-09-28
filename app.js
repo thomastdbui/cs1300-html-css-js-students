9edafc620fba49505a87505dd35edbafd3efdfb6
thomastdbui <thomastdbui@gmail.com>
2022-09-27 20:46:41 -0400

    done?

--- app.js
+++ app.js
@@ -26,15 +26,38 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const names = fruitsArray.filter((item) => {
+    return item.id > 50;
+  })
+
+  const res = names.map((item) => {
+    const newElement = document.createElement('div');
+    newElement.classList.add("name");
+    newElement.innerHTML = item.name;
+
+    const existingElement = document.getElementById('results');
+    existingElement.append(newElement);
+  })
+  // const genus = fruitsArray.map((item) => {
+  //   return item.genus;
+  // })
 
   // TODO: Create a new HTML element to display your data
-
+  // const addElement = () => {
+  //   const newElement = document.createElement('div');
+  //   newElement.classList.add("name");
+  //   newElement.innerHTML = item.name;
+  
   // TODO: Append your new element to the page
-
+  //   const existingElement = document.getElementById('cs1300-gallery');
+  //   existingElement.append(newElement);
+  // }
+  // addElement();
+  
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
